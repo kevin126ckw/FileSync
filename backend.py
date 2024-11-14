f8d4417b0481e4931b7eb4df0565a031e57255c5
kevinchang <35583291@qq.com>
2024-11-14 19:40:23 +0800

    feat(backend/frontend): 实现文件下载和增加服务器列表
    - 添加身份验证（目前非完全体）
    - 优化代码结构和增加调试信息

--- backend.py
+++ backend.py
@@ -1,44 +1,66 @@
 from flask import Flask, render_template, request
 from flask import send_from_directory
 
-import os
+import os,socket
 
-debug = False
+debug = True
 
 app = Flask(__name__)
 
-
-serverips = ['host']
-
 basepath = os.path.dirname(__file__)
+
+
+def auth(Authorization):
+    if Authorization == '1677034306556':
+        return True
+    else:
+        return False
+
 
 @app.route('/upload', methods=['POST', 'GET'])
 def upload():
-    if request.method == 'POST':
-        f = request.files['file']
-        upload_path = '{0}/static/uploads/{1}'.format(basepath, f.filename)
-        f.save(upload_path)
-        return "done" + upload_path
+    if auth(request.headers['Authorization']):
+        if request.method == 'POST':
+            f = request.files['file']
+            upload_path = '{0}/static/uploads/{1}'.format(basepath, f.filename)
+            f.save(upload_path)
+            return "done" + upload_path
 
-
-    return render_template('upload.html')
+        return render_template('upload.html')
+    else:
+        return "403 Illegal access", 403
 
 
 @app.route('/download')
 def download():
-    dir_path = '{0}/static/uploads/'.format(basepath)
-    filename = request.args.get('file')
-    print('downloading ...')
-    return send_from_directory(dir_path, filename, as_attachment=True)
+    if debug:
+        print("headers:", request.headers)
+    if auth(request.headers['Authorization']):
+        dir_path = '{0}/static/uploads/'.format(basepath)
+        filename = request.args.get('file')
+        print('downloading ...')
+        print(dir_path, filename)
+        return send_from_directory(dir_path, filename, as_attachment=True)
+    else:
+        return "403 Illegal access", 403
+
+
 @app.route('/files')
 def files():
-    filelist = []
-    for path in os.listdir('{0}/static/uploads/'.format(basepath)):
-        # check if current path is a file
-        if os.path.isfile(os.path.join('{0}/static/uploads/'.format(basepath), path)):
-            filelist.append(path)
-    return render_template('files.html', filelist=filelist)
+    if debug:
+        print("headers:", request.headers)
+    if auth(request.headers['Authorization']):
+        filelist = []
+        for path in os.listdir('{0}/static/uploads/'.format(basepath)):
+            # check if current path is a file
+            if os.path.isfile(os.path.join('{0}/static/uploads/'.format(basepath), path)):
+                filelist.append(path)
+        return render_template('files.html', filelist=filelist, serverip = request.remote_addr)
+
+    else:
+        return "403 Illegal access", 403
+
 
 
 if __name__ == '__main__':
-    app.run(port=6500,debug=debug)
+    app.run(port=6500, debug=debug)
--- frontend.py
+++ frontend.py
@@ -1,27 +1,36 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, make_response, stream_with_context, Response
 from flask import send_from_directory
 from werkzeug.utils import secure_filename
 import os
 import requests
+import urllib
+from urllib import parse
 
-debug = False
+# --------- CONFIG START ---------
+debug = True
+Authorization = '1677034306556'
+app_port = 5000
+bind_ip = '0.0.0.0'
+header = {
+    'Authorization': Authorization,
+    #'Connection': 'keep-alive',
+    # 'Content-Type': 'multipart/form-data; boundary=----WebKitFormBoundaryFXTT4S1LKA1LUDBd',
+    #'Cookie': 'SHIROJSESSIONID=75ace860-0f00-4db0-9440-6c6d53cdf101',
+    #'Host': 'host:8088',
+    #'Origin': 'http://host:8088',
+    #'Referer': 'http://host:8088/njfxq/search/clue/clueFeedBackDetailAll?id=1574192996457648130&Paramspage=clue'
+    #           '&caseId=1567439544410976257',
+    #'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 '
+    #              'Safari/537.36'
+
+}
+server_list = ['127.0.0.1']
+# --------- CONFIG END ---------
 
 app = Flask(__name__)
 
-header = {
-    'Authorization': '1677034306556',
-    'Connection': 'keep-alive',
-    # 'Content-Type': 'multipart/form-data; boundary=----WebKitFormBoundaryFXTT4S1LKA1LUDBd',
-    'Cookie': 'SHIROJSESSIONID=75ace860-0f00-4db0-9440-6c6d53cdf101',
-    'Host': 'host:8088',
-    'Origin': 'http://host:8088',
-    'Referer': 'http://host:8088/njfxq/search/clue/clueFeedBackDetailAll?id=1574192996457648130&Paramspage=clue&caseId=1567439544410976257',
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36'
-}
-
-serverips = ['127.0.0.1']
-
 basepath = os.path.dirname(__file__)
+
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
@@ -30,21 +39,53 @@ def index():
         upload_path = os.path.join(basepath, 'static', secure_filename(f.filename))
         f.save(upload_path)
         print('uploading ...')
-        # 请求体Payload
         fileObject = {
             'type': (None, '6', None),
             'orgType': (None, 'B', None),
             'file': (f.filename, open(upload_path, 'rb'),
                      'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
         }
-        for serverip in serverips:
-            req = requests.post("http://" + serverip + ':6500/upload', headers=header,
-                            files=fileObject)
-            print("post server ",serverip," back:",req.text)
+        for serverip in server_list:
+            req = requests.post("http://" + serverip + ':6500/upload?file='+request.args.get('file'), headers=header,
+                                files=fileObject)
+            print("post server ", serverip, " back:", req.text)
         print("upload complete")
 
-    return render_template('upload.html',serverips=serverips)
+    return render_template('upload.html', server_list=server_list)
 
+
+@app.route("/files")
+def files():
+    #response = make_response(redirect("http://"+request.args.get('serverip') + ':6500/files'))
+    response = requests.get("http://" + request.args.get('serverip') + ':6500/files', headers=header).text
+    #response.headers['Authorization'] = Authorization
+    #print(response.headers)
+    if debug:
+        type(response)
+        print(response)
+    return response
+
+
+@app.route("/download")
+def download():
+    #response = redirect("http://" + request.args.get('serverip') + ':6500/download')
+    #response = redirect("/test")
+    response = requests.get("http://"+request.args.get('serverip') + ':6500/download?file='+urllib.parse.quote(request.args.get('file')), headers=header, stream=True)
+    response.headers['Authorization'] = Authorization
+    #with app.test_request_context('/download'):
+    #    requests.get(response.location, headers=header)
+    if debug:
+        print(response.headers)
+    #return response
+    back_header = {
+        'Content-Disposition': f'attachment; filename={urllib.parse.quote(request.args.get('file'))}',
+        'Content-Type': response.headers['Content-Type'],
+        'Content-Length': response.headers['Content-Length']
+    }
+    return Response(stream_with_context(response.iter_content()), content_type = response.headers['content-type'], headers=back_header)
+@app.route("/test")
+def test():
+    return str(request.headers)
 
 if __name__ == '__main__':
-    app.run(debug=debug)
+    app.run(debug=debug, port=5000, host=bind_ip)
